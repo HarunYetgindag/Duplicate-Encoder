6cff51820265296aaedbe8b25c51923d20b20101
HarunYetgindag <harunyetgindag@gmail.com>
2022-08-06 13:27:54 +0300

    init commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var result = Solution("recedE");
+
+int test = 2;
+
+static string Solution(string text)
+{
+
+    string[] newString = new string[text.Length];
+
+    var textArray = text.ToArray();
+    for (int i = 0; i < textArray.Length; i++)
+    {
+        var duplicatedChars = textArray.ToList().FindAll(x => String.Equals(textArray[i].ToString(), x.ToString(), StringComparison.OrdinalIgnoreCase));
+        if (duplicatedChars.Count < 2)
+            newString[i] = "(";
+        else
+            newString[i] = ")";
+
+    }
+    return String.Join(" ", newString);
+
+}
